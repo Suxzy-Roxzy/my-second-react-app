fb64d4aa3959239e43ad4fc418d5e33d0e5e6e42
Success Abani <successabani@gmail.com>
2025-03-15 16:55:32 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h2>Functional arrow Component</h2>
+      <h2>What a goal!!!!!</h2>
+      <h1>Ride on</h1>
+    </>
+  )
+};
+
+export default App;
